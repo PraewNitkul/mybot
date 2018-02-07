0c3f57ea5f977f3f3069d7ec598111f06bcafaa2
PraewNitkul <36221908+PraewNitkul@users.noreply.github.com>
2018-02-07 14:54:44 +0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -49,6 +49,13 @@ if (!is_null($events['events'])) {
 					'text' => 'support u'
 				];
 			}
+			else if($text == 's'){
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => 'fight!!'
+				];
+			}
 		
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
